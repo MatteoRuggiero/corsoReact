1d0af7eb04d7e0ce4760bec1547de5e0f9a21b46
Mauro Semproni <mauro.semproni@webturing.net>
2024-06-04 16:56:48 +0200

    aggiustamenti navbar bootstrap

--- routing-tipo-archibus/src/components/navbar.jsx
+++ routing-tipo-archibus/src/components/navbar.jsx
@@ -1,80 +1,41 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import './navbar.css'
+import { Outlet } from "react-router";
 
 const Navbar = () => {
-  const [window, setWindow] = useState(false);
 
-  let openClose = () => {
-    if (window === false) {
-      setWindow(true);
-    } else {
-      setWindow(false);
-    }
-  };
+
   return (
-    <nav className="navbar-menu" style={{ width: window === false ? 250 : 60 }}>
-      <div className="burger" onClick={() => openClose()}>
-        <img src="vite.svg" alt="burger" />
-      </div>
-      <ul className="navbar__list">
-          <div className="navbar__li-box" >
-            <img
-              src="vite.svg"
-              style={{ paddingLeft: window === false ? 27 : 17 }}
-            />
-            <li
-              className="navbar__li"
-              style={{ display: window === false ? "inline-block" : "none" }}
-            >
-              <NavLink
-                  to="/Lorenzo"
-                  className={({ isActive }) =>
-                    isActive ? "link active mx-3" : "link mx-3"
-                  }
-             >
-              Lorenzo
-              </NavLink>
-            </li>
-          </div> <div className="navbar__li-box" >
-            <img
-              src="vite.svg"
-              style={{ paddingLeft: window === false ? 27 : 17 }}
-            />
-            <li
-              className="navbar__li"
-              style={{ display: window === false ? "inline-block" : "none" }}
-            >
-              <NavLink
-                  to="/Mauro"
-                  className={({ isActive }) =>
-                    isActive ? "link active mx-3" : "link mx-3"
-                  }
-             >
-              Mauro
-              </NavLink>
-            </li>
-          </div> <div className="navbar__li-box" >
-            <img
-              src="vite.svg"
-              style={{ paddingLeft: window === false ? 27 : 17 }}
-            />
-            <li
-              className="navbar__li"
-              style={{ display: window === false ? "inline-block" : "none" }}
-            >
-              <NavLink
-                  to="/Matteo"
-                  className={({ isActive }) =>
-                    isActive ? "link active mx-3" : "link mx-3"
-                  }
-             >
-              Matteo
-              </NavLink>
-            </li>
-          </div>
-      </ul>
-    </nav>
+    <>
+    <div className="container-fluid">
+        <div className="row flex-nowrap">
+            <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+                <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                    <NavLink to="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                        <span className="fs-5 d-none d-sm-inline">Home</span>
+                    </NavLink>
+                    <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <li>
+                            <NavLink to="/Lorenzo" className="px-0 align-middle">
+                                <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Lorenzo</span></NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Matteo" className="px-0 align-middle">
+                                <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Matteo</span></NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Mauro" className="px-0 align-middle">
+                                <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Mauro</span></NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="col py-3">
+                <Outlet />
+            </div>
+        </div>
+    </div>
+
+    </>
   );
 };
 
